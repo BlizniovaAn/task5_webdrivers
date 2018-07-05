5120a59ad2827d50d183c00d9bbdb2dd9324c2f4
hanna_blizniova <hanna_blizniova@epam.com>
2018-07-05 17:10:48 +0300

    T5 - Add test for sending emails

---
+++ src/test/java/com/epam/cdp/Constants.java
@@ -0,0 +1,7 @@
+package com.epam.cdp;
+
+public class Constants {
+
+    public static final String OPEN_DRAFT_FOLDER_URL = "https://mail.google.com/mail/u/0/#drafts";
+    public static final String DRAFT = "Черновики";
+}
